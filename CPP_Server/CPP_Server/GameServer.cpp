0183fd40fdb03c1f19aeb572710fd328179b9cfa
jeong98CH <jchul1998@naver.com>
2022-02-07 17:47:59 +0900

    future basic

--- CPP_Server/CPP_Server/GameServer.cpp
+++ CPP_Server/CPP_Server/GameServer.cpp
@@ -1,72 +1,87 @@
 #include "pch.h"
+
 #include <iostream>
 using namespace std;
-#include "CorePch.h"
 
+#include "CorePch.h"
 #include <thread>
 #include<atomic>
 #include <mutex>
-#include <Windows.h>
+#include <future>
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
+int64 result = 0;
 
-//참고) CV는 User_Level Object이다.
-condition_variable cv;
-
-void Producer()
+int64 Calculate()
 {
-	while (true)
-	{
-		// 1) lock을 잡고
-		// 2) 공유 변수 값을 수정
-		// 3) Lock 해제
-		// 4) 조건변수 통해 다른 쓰레드에 통지
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
-		 
-		////handle를 signal 상태로 바꿔준다
-		//::SetEvent(handle);
-		cv.notify_one();
+	int64 sum = 0;
+	for (int32 i = 0; i < 100'000; i++)
+		sum += i;
 
-		/*this_thread::sleep_for(100ms);*/
-	}
+	return result = sum;
 }
 
-void Consumer()
+void PromiseWorker(::promise<string>&& promise)
 {
-	while (true)
-	{
-		//::WaitForSingleObject(handle, INFINITE); //무한대기
-		
-		unique_lock<mutex> lock(m);
-		cv.wait(lock, []() { return q.empty() == false; });
+	promise.set_value("Secret Message");
+}
 
-		if (q.empty() == false)
-		{
-			int32 data = q.front();
-			q.pop();
-			cout << q.size() << endl;
-		}
-	}
+void TaskWorkder(::packaged_task<int64(void)>&& task)
+{
+	task();
+
+	//return이 없는 이유는...
+	// task의 실행결과인 int64는 future를 통해 받아오게 되기 때문
 }
 
 int main()
 {
-	//HANDLE 구조는 정수형 데이터이지만, Event의 ID와 같은 역활을 한다.
-	handle = ::CreateEvent(NULL/*보안속성*/,
-							FALSE /*수동 리셋여부 (false => Auto)*/,
-							FALSE/*초기화상태*/,
-							NULL/*이름*/);
+	//std::future : 언젠가 '미래'에 값을 뱉어주겠다.
+	{
+		//async방식 - 멀티 쓰레드 환경이됨
+		//deferred -  단일 쓰레드 환경임
+		::future<int64> future = ::async(::launch::async, Calculate);
+		//::future<int64> future = ::async(::launch::deferred, Calculate);
 
-	thread t1(Producer);
-	thread t2(Consumer);
+		//todo
 
-	t1.join();
-	t2.join();
+		int64 sum = future.get(); // 이제 결과물이 필요하다.
+	}
 
-	::CloseHandle(handle);
+	//std::promise
+	{
+		//미래에 결과물을 반환할 것이라는 '약속'
+		::promise<string> promise;
+		
+		future<string> future = promise.get_future();
+		
+		/*
+		promise 는 다른 thread에,
+		future는 우리가 가지고 있는 형태
+		
+		무전기처럼 promise와 연동..
+		만약 다른 thread에서 promise의 데이터를 수정한다면
+		future.get()으로 가져올 수 있음
+		*/
+
+		thread t(PromiseWorker, ::move(promise));
+		t.join();
+
+		string str = future.get();
+		cout << str << endl;
+	}
+	
+	//std::packaged_task
+	{
+		//'함수의 시그니처'를 맞추어 생성해야함!
+		::packaged_task<int64(void)> task(Calculate);
+	
+		::future<int64> future = task.get_future();
+
+		thread t1(TaskWorkder, ::move(task));
+		t1.join();
+
+		int64 sum= future.get();
+		cout << sum << endl;
+
+	}
 }
