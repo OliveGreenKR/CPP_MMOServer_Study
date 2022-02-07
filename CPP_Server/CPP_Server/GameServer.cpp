057cc683c4963cf3048f73c5821e25004b7346fd
jeong98CH <jchul1998@naver.com>
2022-02-07 18:13:04 +0900

    캐시 테스트

--- CPP_Server/CPP_Server/GameServer.cpp
+++ CPP_Server/CPP_Server/GameServer.cpp
@@ -9,79 +9,61 @@ using namespace std;
 #include <mutex>
 #include <future>
 
-int64 result = 0;
+#include <Windows.h>
 
-int64 Calculate()
-{
-	int64 sum = 0;
-	for (int32 i = 0; i < 100'000; i++)
-		sum += i;
-
-	return result = sum;
-}
-
-void PromiseWorker(::promise<string>&& promise)
-{
-	promise.set_value("Secret Message");
-}
-
-void TaskWorkder(::packaged_task<int64(void)>&& task)
-{
-	task();
-
-	//return이 없는 이유는...
-	// task의 실행결과인 int64는 future를 통해 받아오게 되기 때문
-}
+int32 buffer[10000][10000];
 
 int main()
 {
-	//std::future : 언젠가 '미래'에 값을 뱉어주겠다.
+	::memset(buffer, 0, sizeof(buffer));
+
 	{
-		//async방식 - 멀티 쓰레드 환경이됨
-		//deferred -  단일 쓰레드 환경임
-		::future<int64> future = ::async(::launch::async, Calculate);
-		//::future<int64> future = ::async(::launch::deferred, Calculate);
 
-		//todo
+		uint64 start = GetTickCount64();
+		int64 sum = 0;
 
-		int64 sum = future.get(); // 이제 결과물이 필요하다.
+		for (int32 i = 0; i < 10000; i++)
+		{
+			for (int32 j = 0; j < 10000; j++)
+			{
+				sum += buffer[i][j];
+			}
+		}
+
+		uint64 end = GetTickCount64();
+
+		cout << "Elapsed Tick : " << end - start << endl;
 	}
 
-	//std::promise
 	{
-		//미래에 결과물을 반환할 것이라는 '약속'
-		::promise<string> promise;
-		
-		future<string> future = promise.get_future();
-		
-		/*
-		promise 는 다른 thread에,
-		future는 우리가 가지고 있는 형태
-		
-		무전기처럼 promise와 연동..
-		만약 다른 thread에서 promise의 데이터를 수정한다면
-		future.get()으로 가져올 수 있음
-		*/
+		uint64 start = GetTickCount64();
+		int64 sum = 0;
 
-		thread t(PromiseWorker, ::move(promise));
-		t.join();
+		for (int32 i = 0; i < 10000; i++)
+		{
+			for (int32 j = 0; j < 10000; j++)
+			{
+				sum += buffer[j][i];
+			}
+		}
 
-		string str = future.get();
-		cout << str << endl;
+		uint64 end = GetTickCount64();
+
+		cout << "Elapsed Tick : " << end - start << endl;
 	}
-	
-	//std::packaged_task
-	{
-		//'함수의 시그니처'를 맞추어 생성해야함!
-		::packaged_task<int64(void)> task(Calculate);
-	
-		::future<int64> future = task.get_future();
 
-		thread t1(TaskWorkder, ::move(task));
-		t1.join();
-
-		int64 sum= future.get();
-		cout << sum << endl;
-
-	}
+	//2번째 방법이 3배정도 느리다..
+	/*
+	* 바로 그것이 cache의 동작 때문이다..
+	*
+	* *캐시가 데이터를 가져올 때,  캐시 철학에 따라,  주변의 데이터들을 블록 단위로 가져오게 된다.
+	*
+	* --> 그 후에 근처의 값을 사용할 때, 만약 이미 캐시에 있다면--> cache hit 이 발생하여 속도가 매우 빠르다..
+	*
+	*
+	*
+	*	>> 두 번 째 방법은 처음 buffer[0][0]을 가져올 때, 주변의 것들 [0][1] / [0][2] .. 들을 가져온다.
+	*	   따라서 cache hit이 자주 발생하지 않고, 렘까지 가야하기 때문에 발동 시간이 오래 걸리는 것
+	*
+	*/
 }
