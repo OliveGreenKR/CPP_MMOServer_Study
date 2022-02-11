c1fb1e0c3d9ad9eabe4991a72fef0cef35135c19
jeong98CH <jchul1998@naver.com>
2022-02-11 12:03:58 +0900

    threadManger & CoreManger

--- CPP_Server/CPP_Server/GameServer.cpp
+++ CPP_Server/CPP_Server/GameServer.cpp
@@ -12,7 +12,27 @@ using namespace std;
 #include <stack>
 #include <Windows.h>
 
+#include "ThreadManager.h"
+CoreGlobal Core;
+
+
+void ThreadMain()
+{
+	while (true)
+	{
+		cout << "hello I am Thread ..." <<
+			LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
 int main()
 {
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch(ThreadMain);
+	}
+
+	GThreadManager->Join();
+
 
 }
---
+++ CPP_Server/ServerCore/CoreTLS.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//threadid 관리를 위한 "TLS" extern변수
+extern thread_local uint32 LThreadId; 
+
