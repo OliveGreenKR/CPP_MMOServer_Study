6ffcf1da3bef5919c38b530e1b3d490f5ee3254b
jeong98CH <jchul1998@naver.com>
2022-02-11 10:52:23 +0900

    remove lock free

--- CPP_Server/CPP_Server/GameServer.cpp
+++ CPP_Server/CPP_Server/GameServer.cpp
@@ -8,69 +8,11 @@ using namespace std;
 #include<atomic>
 #include <mutex>
 #include <future>
-
 #include <queue>
 #include <stack>
-
 #include <Windows.h>
-
-#include "ConcurrentQueue.h"
-#include "ConcurrentStack.h"
-
-LockQueue<int32> q;
-LockFreeStack<int32> s;
-
-#define SHARED
-
-#ifdef POPCOUNT
-void Push()
-{
-	while (true)
-	{
-		int32 value = rand() % 100;
-		s.Push(value);
-	}
-}
-void Pop()
-{
-	while (true)
-	{
-		int32 data = 0;
-		s.TryPop(data);
-		cout << data << endl;
-	}
-}
-#endif 
-
-#ifdef SHARED
-void Push()
-{
-	while (true)
-	{
-		int32 value = rand() % 100;
-		s.Push(value);
-	}
-}
-void Pop()
-{
-	while (true)
-	{
-		auto data = s.TryPop();
-		if (data != nullptr)
-			cout << (*data) << endl;
-	}
-}
-#endif // SHARED
-
-
 
 int main()
 {
-	thread t1(Push);
-	thread t2(Pop);
-	
-	t1.join();
-	t2.join();
-	
 
 }
