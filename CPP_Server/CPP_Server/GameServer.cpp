0e316073a89d5ed333f480c54e35e29bdb882c7f
jeong98CH <jchul1998@naver.com>
2022-03-03 18:39:40 +0900

    STLHEADER

--- CPP_Server/CPP_Server/GameServer.cpp
+++ CPP_Server/CPP_Server/GameServer.cpp
@@ -13,23 +13,23 @@
 #include "RefCounting.h"
 #include "Memory.h"
 #include "Allocator.h"
-#include "LockFreeStack.h"
+
 
 DECLSPEC_ALIGN(16)
 class Data
 {
 public:
-	SListEntry _entry;
+	SLIST_ENTRY _entry;
 	int64 _rand = rand() % 1000;
 };
 
-SListHeader* GHeader;
+SLIST_HEADER* GHeader;
 
 int main()
 {
-	GHeader = new SListHeader();
+	GHeader = new SLIST_HEADER();
 	ASSERT_CRASH(((uint64)GHeader % 16) == 0);
-	InitializeHeader(GHeader);
+	InitializeSListHead(GHeader);
 
 	for (int32 i = 0; i < 3; i++)
 	{
@@ -40,7 +40,7 @@ int main()
 					Data* data = new Data();
 					ASSERT_CRASH(((uint64)data % 16) == 0);
 
-					PushEntrySList(GHeader, (SListEntry*)data);
+					InterlockedPushEntrySList(GHeader, (SLIST_ENTRY*)data);
 					this_thread::sleep_for(10ms);
 				}
 			});
@@ -53,7 +53,7 @@ int main()
 				while (true)
 				{
 					Data* pop = nullptr;
-					pop = (Data*)PopEntrySList(GHeader);
+					pop = (Data*)InterlockedPopEntrySList(GHeader);
 
 					if (pop)
 					{
