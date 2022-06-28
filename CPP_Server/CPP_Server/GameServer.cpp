89977e9c0251e918d518ff970e93f4953a439702
jeong98CH <jchul1998@naver.com>
2022-06-28 14:41:03 +0900

    Non blocking TDP

--- CPP_Server/CPP_Server/GameServer.cpp
+++ CPP_Server/CPP_Server/GameServer.cpp
@@ -22,75 +22,84 @@ void HandleError(const char* cause)
 }
 int main()
 {
-    //WinSock Lib Init
-    WSAData wsaData;
-    if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-        return 0;
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-    //socekt
-    SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
-    if (serverSocket == INVALID_SOCKET)
-    {
-        HandleError("Socekt");
-        return 0;
-    }
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+		return 0;
 
-    //옵션을 해석하고 처리하는 주체 : level
-    //소켓코드 -> SOL_SOCKERT
-    //IPv4 -> IPPROTO_IP
-    //TCP 프로토콜 -> IPPROTO_TCP
+	u_long on = 1;
+	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
+		return 0;
 
-    //해당 level의 option 설정 :  opt
-    //다양한 옵션이 존재. MS DOC 참고  -> 각 옵션마다 맏는 optval이 다르기 때문에 꼭 확인하자
-    
-    //SO_KEEPALIVE = 주기적으로 연결 상태 확인 여부(TCP_ONLY)
-    //상대방이 연결을 일방적으로 끊는 경우에 대한 대처가 가능
-    //주기적으로 연결상태를 확인하는 패킷을 보내서 끊어진 연결을 감지한다.
-    bool enable = true;
-    setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	serverAddr.sin_port = ::htons(7777);
 
-    //SO_LINGER : 지연하다
-    // 소켓 리소스 반환 전에 보냈던 senddata의 처리 여부
-    // 송신버퍼에 있는 데ㅐ이터를 보낼것인가?
-    LINGER linger;
-    // linger onoff(default = 0) : 
-    // 0 이면 closesocket()이 바로 리턴;
-    // 1 이면 linger sec 만큼 대기 후 리턴;
-    linger.l_onoff = 1;
-    linger.l_linger = 5; //5 sec
-    setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger , sizeof(linger));
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		return 0;
 
+	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+		return 0;
 
-    //HALF-CLOSE
-    //SD_SEND : send 막는다.
-    //SD_RECEIVE : recv 막는다
-    //SD_BOTH :  둘다 막는다.
-    //::shutdown(serverSocket, SD_SEND);
+	cout << "Accept" << endl;
 
-    // SO_SNDBUF, SO_RCVBUF : 송수신 버퍼 크기관련 사용.
-    int32 sendBufferSize;
-    int32 optionLen = sizeof(sendBufferSize);
-    ::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLen);
+	SOCKADDR_IN clientAddr;
+	int32 addrLen = sizeof(clientAddr);
 
-    //SO_REUSEADDR : IP주소 및 Port를 재사용
-    {
-        bool enable = true;
-        ::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
-    }
+	// Accept
+	while (true)
+	{
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			// Error
+			break;
+		}
+		cout << "Client Connected!" << endl;
 
-    //IPPROTO_TCP 
-    //TCP_NODELAY : Nagle 알고리즘 작동여부
-    //*Nagle : 데이터가 충분히 크면 보내고, 그렇지 않으면 쌓일 때까지 대기.
-    //==> 불필요한 packet 생성 감소, But 대기하면서 반응 시간 손해.
-    //게임 같은 고반응성 서버에서는 꼭 꺼주는 것이 좋다!!
-    {
-        bool enable = true;
-        ::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
-    }
-
-    //소켓 리소스 반환
-    ::closesocket(serverSocket); 
-
-    //winsock 종료
-    ::WSACleanup();
+		// Recv
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+				// Error
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				// 연결 끊김
+				break;
+			}
+			cout << "Recv Data Len = " << recvLen << endl;
+			// Send
+			while (true)
+			{
+				if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR)
+				{
+					// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
+					if (::WSAGetLastError() == WSAEWOULDBLOCK)
+						continue;
+					// Error
+					break;
+				}
+				cout << "Send Data ! Len = " << recvLen << endl;
+				break;
+			}
+		}
+	}
+	// 윈속 종료
+	::WSACleanup();
 }
