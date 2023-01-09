a5f894aecbfbecb5c9c241316e36547b64e44a9d
OliveGreen <olivegreenkr98@gmail.com>
2023-01-09 18:38:57 +0900

    path

--- CPP_Server/Tools/PacketGenerator/PacketGenerator.py
+++ CPP_Server/Tools/PacketGenerator/PacketGenerator.py
@@ -5,7 +5,7 @@ import ProtoParser
 def main():
 
 	arg_parser = argparse.ArgumentParser(description = 'PacketGenerator')
-	arg_parser.add_argument('--path', type=str, default='C:/Rookiss/CPP_Server/Server/Common/Protobuf/bin/Protocol.proto', help='proto path')
+	arg_parser.add_argument('--path', type=str, default='C:/OliveGreen/CPP_Server/CPP_Server/Common/Protobuf/bin/Protocol.proto', help='proto path')
 	arg_parser.add_argument('--output', type=str, default='TestPacketHandler', help='output file')
 	arg_parser.add_argument('--recv', type=str, default='C_', help='recv convention')
 	arg_parser.add_argument('--send', type=str, default='S_', help='send convention')
@@ -13,6 +13,7 @@ def main():
 
 	parser = ProtoParser.ProtoParser(1000, args.recv, args.send)
 	parser.parse_proto(args.path)
+
 	file_loader = jinja2.FileSystemLoader('Templates')
 	env = jinja2.Environment(loader=file_loader)
 
